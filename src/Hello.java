dead52363374aaf779f088f1443bc1a251dde87b
Admin-PC\Admin <kyend1@naver.com>
2018-05-14 14:00:05 +0900

    TYUTXYUX

--- src/Hello.java
+++ src/Hello.java
@@ -2,5 +2,10 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("이것은 테스트 입니다.");
+		
+		//TODO 뭘해야할지 모르겠네염
+		//DDFSDFSD
+		//ASDSAD
+		System.out.println();
 	}
 }
--- src/TestDD.java
+++ src/TestDD.java
@@ -3,6 +3,6 @@ public class TestDD {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello Wordl");
-		
+		//SDASDASDS
 	}
 }
